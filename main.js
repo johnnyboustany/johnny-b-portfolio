d746b429d80d7b23aaabe0caaabbbeb2b8a27510
Johnny Boustany <Johnny_Boustany@Brown.edu>
2022-10-12 01:07:53 -0400

    m
    m

--- main.js
+++ main.js
@@ -228,8 +228,8 @@ var requestID;
 function resizeRendererToDisplaySize(renderer) {
 
     const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
     const needResize = canvas.width !== width || canvas.height !== height;
     if (needResize) {
         renderer.setSize(width, height, false);
@@ -240,16 +240,28 @@ function resizeRendererToDisplaySize(renderer) {
 }
 
 
+// window.addEventListener('resize',function(){
+//
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//
+//     if(!isMobile()){
+//         johnny.position.z = -25;
+//         johnny.position.x = 15;
+//     } else {
+//         johnny.position.z = -60;
+//         johnny.position.x = 14;
+//     }
+// });
 function animate() {
   requestID = requestAnimationFrame(animate);
 
     if (resizeRendererToDisplaySize(renderer)) {
-        const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
     }
-
-
+    
     // Animates the water
     water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
 
@@ -338,20 +350,7 @@ const home = document.getElementById('home-a');
 });
 
 
-window.addEventListener('resize',function(){
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    if(!isMobile()){
-        johnny.position.z = -25;
-        johnny.position.x = 15;
-    } else {
-        johnny.position.z = -60;
-        johnny.position.x = 14;
-    }
-});
 
 
 
